30ccd04ad1e82271bdff0cb7e56e4b2ca444acd3
whiue-nhh12 <hieuhuu0609@gmail.com>
2025-04-08 10:31:04 +0700

    add file

--- defs.h
+++ defs.h
@@ -2,7 +2,7 @@
 #define _DEFS__H
 const int SCREEN_WIDTH = 1400;
 const int SCREEN_HEIGHT = 600;
-const int OBSTACLE_SPEED = 15;
+const int OBSTACLE_SPEED = 20;
 const  int OBSTACLE_GAP = 150;
 const int OBSTACLE_SPAWN_RATE = 70;
 const int OBSTACLE_WIDTH=140;
